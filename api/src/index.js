7c560278119014ca1f0d5fddc39c602c6be73e8c
Maxim Zeyfert <maxim.zeyfert@ringcentral.com>
2021-03-29 13:21:05 +0300

    First version of the project is created

---
+++ api/src/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+let { port, db } = require('./variables');
+
+const app = express();
+
+const statisticsScheme = mongoose.Schema(
+  { City: String, WebStudies: Number, Hosting: Number, University: Number},
+  { collection: 'data' }
+);
+
+const peopleScheme = mongoose.Schema(
+  { City: String, People: Number},
+  { collection: 'number' }
+);
+
+const statisticsModel = mongoose.model('data', statisticsScheme);
+
+const statisticsModelPeople = mongoose.model('number', peopleScheme);
+
+app.get('/api/number', (req, res) => {
+  statisticsModel.find({}, (err, data) => {
+    if (err) { return console.log(err) }
+    res.send(data);
+  });
+});
+
+app.get('/api/people', (req, res) => {
+  statisticsModelPeople.find({}, (err, data) => {
+    if (err) { return console.log(err) }
+    res.send(data);
+  });
+});
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if (err) { return console.log(err) }
+  app.listen(port, () => {
+    console.log(`Server is working on ${port}`);
+  });
+});
